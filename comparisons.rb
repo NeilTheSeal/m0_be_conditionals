ba215ab4a9878dd56b36520c3c08990c82e4d769
Neil Hendren <neil.hendren@gmail.com>
2024-01-11 20:23:43 -0700

    Complete comparisons exercise

--- comparisons.rb
+++ comparisons.rb
@@ -23,24 +23,31 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
+puts "Is number_teachers less than number_students?\n#{number_teachers < number_students}";
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
+puts "Is number teachers equal to string_teachers?\n#{string_teachers == number_teachers}";
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
+puts "Is number_teachers not equal to number_students?\n#{number_teachers != number_students}";
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
+puts "Is number_students greater than or equal to 20?\n#{number_students >= 20}";
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
+puts "Is number_students greater than or equal to 21?\n#{number_students >= 21}";
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
+puts "Is number_students less than or equal to 20?\n#{number_students <= 20}";
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
+puts "Is number_students less than or equal to 21?\n#{number_students <= 21}";
 
 
 #-------------------
@@ -53,21 +60,21 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Make sure YOU can explain it that way!
 
 puts 4 < 9
-#YOU DO: Explain.
+# This is comparing the value of the number 4 and the number 9, to see if 4 is less than 9. It will print a boolean in the terminal, with a value of true
 
 books = 3
 puts 4 < books
-# YOU DO: Explain.
+# This is comparing the number 4 with the value of the variable "books", which is 3, to see if 4 is less than "books". It will print a boolean in the terminal, with a value of false
 
 friends = 6
 siblings = 2
 puts friends > siblings
-# YOU DO: Explain.
+# This is comparing the value of the variable "friends" with the value of the variable "siblings", to see if "friends" is greater than "siblings". It will print a boolean in the terminal, with a value of true.
 
 attendees = 9
 meals = 8
 puts attendees != meals
-# YOU DO: Explain.
+# This is checking to see if the value of the variable "attendees" is NOT equal to the value of the variable "meals". It will print a boolean to the terminal with a value of true.
 
 
 #-------------------
@@ -90,15 +97,17 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play and loves_treats;
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park;
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play or loves_dog_park;
 
 # Determine if the dog loves to play and is a puppy
+puts loves_to_play && age <= 1;
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
 # ANSWER:
+# My final line of code evaluated to "true", because loves_to_play is a boolean with the value "true" and the dog's age is less than or equal to 1. I consider a puppy to be a dog with the age of 1 or lower.
