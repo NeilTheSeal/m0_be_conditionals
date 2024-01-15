3f1102480463bc10d58e1ddd31ddd996df8d0b4c
Neil Hendren <neil.hendren@gmail.com>
2024-01-15 14:21:33 -0700

    Add methods to fizz_buzz.rb

--- code_challenges/fizz_buzz.rb
+++ code_challenges/fizz_buzz.rb
@@ -9,3 +9,41 @@ elsif fizzbuzz_integer % 5 == 0
 else
     puts fizzbuzz_integer
 end
+
+# Extra credit (because I am bored)
+
+# 1.) A method that does FizzBuzz and a for loop over 20 integers
+def fizzbuzz(num)
+    if num % 3 == 0 && num % 5 == 0
+        return "FizzBuzz"
+    elsif num % 3 == 0
+        return "Fizz"
+    elsif num % 5 == 0
+        return "Buzz"
+    else
+        return "#{num}"
+    end
+end
+
+for n in 1..20
+    puts fizzbuzz(n)
+end
+
+# 2.) An alternate way of doing FizzBuzz
+def fizzbuzz2(num)
+    str = ""
+    if num % 3 == 0
+        str += "Fizz"
+    end
+    if num % 5 == 0
+        str += "Buzz"
+    end
+    if num % 3 != 0 && num % 5 != 0
+        str += "#{num}"
+    end
+    return str
+end
+
+for n in 1..20
+    puts fizzbuzz2(n)
+end
